d174508c4549e788ecf6c8499b407b5a1cf316a0
Princedewangan101 <dewanganprince5151@gmail.com>
2026-07-07 16:14:48 +0530

    add: signup logic

--- backend/src/handlers/signup.rs
+++ backend/src/handlers/signup.rs
@@ -1,3 +1,27 @@
-fn signin() ->  {
-    
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
 }
+
+#[derive(Serialize)]
+pub struct SignupResponse {
+    pub message: String,
+    pub success: bool,
+}
+
+pub async fn signup(Json(req):Json<SignupRequest>) -> impl IntoResponse {
+
+
+    
+    let response_body = SignupResponse {
+        success: true,
+        message: format!("User {}: {} registered successfully!", req.email, req.password),
+    };
+
+    (StatusCode::CREATED, Json(response_body))
+}
+
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,16 @@
-use axum::{Router, routing::get};
+use axum::{Router, routing::post};
+
+mod handlers;
+
+use handlers::signup::signup;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/bar", get(foo_bar));
+        .route("/api/signup", post(signup));
+        
 
-    async fn get_foo() {}
-    async fn post_foo() {}
-    async fn foo_bar() {}
-
-    
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
     println!("> server running on port 5000");
     axum::serve(listener, app).await.unwrap();
