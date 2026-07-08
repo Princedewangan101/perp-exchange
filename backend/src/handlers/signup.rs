00a139044c48ff5f1f7385dccb53b93de04b3eb8
Princedewangan101 <dewanganprince5151@gmail.com>
2026-07-08 18:15:26 +0530

    add: signup route

--- backend/src/handlers/signup.rs
+++ backend/src/handlers/signup.rs
@@ -1,10 +1,15 @@
-use axum::{Json, http::StatusCode, response::IntoResponse};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 use serde::{Deserialize, Serialize};
+
+use crate::{
+    DbState,
+    query::query::{UserStatusResponse, is_user_exist},
+};
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
     pub email: String,
-    pub password: String,
+    // pub password: String,
 }
 
 #[derive(Serialize)]
@@ -13,15 +18,35 @@ pub struct SignupResponse {
     pub success: bool,
 }
 
-pub async fn signup(Json(req):Json<SignupRequest>) -> impl IntoResponse {
+pub async fn signup(
+    State(pg_client): State<DbState>,
+    Json(req): Json<SignupRequest>,
+) -> impl IntoResponse {
+    let response: UserStatusResponse = is_user_exist(&pg_client, &req.email).await;
 
+    let response_body: SignupResponse;
 
-    
-    let response_body = SignupResponse {
-        success: true,
-        message: format!("User {}: {} registered successfully!", req.email, req.password),
-    };
+    if !response.is_user_exist {
 
-    (StatusCode::CREATED, Json(response_body))
+        // hash password
+        // create user
+        // sign jwt
+        // set jwt in cookie
+        // send response
+
+        response_body = SignupResponse {
+            success: true,
+            message: format!(       // ??? difference between format! and println!
+                "User {}: registered successfully!",
+                response.email.as_deref().unwrap_or("No email found")
+            ),
+        };
+        (StatusCode::CREATED, Json(response_body))
+    } else {
+        response_body = SignupResponse {
+            success: true,
+            message: format!("User {}: already exist!", req.email),
+        };
+        (StatusCode::CREATED, Json(response_body))
+    }
 }
-
